d7b5ecf280409aa5808a268477e186898069ab83
lizhigaomac <lizhigao@021.com>
2017-12-18 01:08:05 +0800

    init

---
+++ src/reducers/index.js
@@ -0,0 +1,65 @@
+import { ADD_RECIPE, REMOVE_FROM_CALENDAR } from '../actions';
+
+const initialCalendarState = {
+  sunday: {
+    breakfast: null,
+    launch: null,
+    dinner: null
+  },
+  monday: {
+    breakfast: null,
+    launch: null,
+    dinner: null
+  },
+  tuesday: {
+    breakfast: null,
+    launch: null,
+    dinner: null
+  },
+  wednesday: {
+    breakfast: null,
+    launch: null,
+    dinner: null
+  },
+  thursday: {
+    breakfast: null,
+    launch: null,
+    dinner: null
+  },
+  friday: {
+    breakfast: null,
+    launch: null,
+    dinner: null
+  },
+  saturday: {
+    breakfast: null,
+    launch: null,
+    dinner: null
+  }
+};
+
+function calendar(state = initialCalendarState, action) {
+  const { day, recipe, meal } = action;
+  switch (action.type) {
+    case ADD_RECIPE:
+      return {
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: recipe.label
+        }
+      };
+    case REMOVE_FROM_CALENDAR:
+      return {
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: null
+        }
+      };
+    default:
+      return state;
+  }
+}
+
+export default calendar;
