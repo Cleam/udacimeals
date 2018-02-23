03ed6a608da400f6d3676b8a10693aa27b9669d1
lizhigaomac <lizhigao@021.com>
2018-02-23 22:44:44 +0800

    learn combineReducers

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { createStore } from 'redux';
 import reducer from './reducers';
 import './index.css';
 import App from './components/App';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(
@@ -11,5 +12,10 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDOM.render(<App store={store} />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
